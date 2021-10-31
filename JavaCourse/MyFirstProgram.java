983ae0a417e2c02fa3f41ca3f80b6ff7f8ad88a7
Emilio Sanchez <79680207+ElCopias@users.noreply.github.com>
2021-10-30 20:44:48 -0500

    Add files via upload

---
+++ JavaCourse/MyFirstProgram.java
@@ -0,0 +1,20 @@
+package src;
+
+public class MyFirstProgram {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		System.out.println("How are you today?");
+	}
+	
+	//This is a comment
+	
+	/*
+	 * This
+	 * is 
+	 * a 
+	 * large
+	 * comment
+	 */						
+}
